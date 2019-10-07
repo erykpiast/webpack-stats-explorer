3a8f687d051df0b594f11da8a8e19715b81e4139
Eryk Napierała <eryk.piast@gmail.com>
2019-10-07 22:33:14 +0200

    feat(testing): allow to upgrade deps of all playgrounds

--- playground/play.sh
+++ playground/play.sh
@@ -6,14 +6,24 @@ OLD_PWD=$(pwd);
 EXAMPLE=$1;
 VERSIONS=("a" "b" "c" "d" "e" "f");
 
-for VERSION in ${VERSIONS[*]}; do
-    DIR="$OLD_PWD/playground/$EXAMPLE/$VERSION";
+if [ -z $EXAMPLE ]; then
+    EXAMPLES=$(find playground -mindepth 1 -maxdepth 1 -type d -exec basename {} \;);
+else
+    EXAMPLES=("$EXAMPLE");
+fi;
 
-    if [[ -d "$DIR" ]]; then
-        cd "$DIR";
-        [[ -d "./node_modules" ]] || npm ci;
-        npm run build;
-    fi;
+for EXAMPLE in ${EXAMPLES[@]}; do
+    echo "Playing with $EXAMPLE";
+
+    for VERSION in ${VERSIONS[*]}; do
+        DIR="$OLD_PWD/playground/$EXAMPLE/$VERSION";
+
+        if [[ -d "$DIR" ]]; then
+            cd "$DIR";
+            [[ -d "./node_modules" ]] || npm ci;
+            npm run build;
+        fi;
+    done;
 done;
 
 cd "$OLD_PWD";
--- playground/upgrade-deps.sh
+++ playground/upgrade-deps.sh
@@ -6,14 +6,24 @@ OLD_PWD=$(pwd);
 EXAMPLE=$1;
 VERSIONS=("a" "b" "c" "d" "e" "f");
 
-for VERSION in ${VERSIONS[*]}; do
-    DIR="$OLD_PWD/playground/$EXAMPLE/$VERSION";
+if [ -z $EXAMPLE ]; then
+    EXAMPLES=$(find playground -mindepth 1 -maxdepth 1 -type d -exec basename {} \;);
+else
+    EXAMPLES=("$EXAMPLE");
+fi;
 
-    if [[ -d "$DIR" ]]; then
-        cd "$DIR";
-        ncu -u;
-        npm i;
-    fi;
+for EXAMPLE in ${EXAMPLES[@]}; do
+    echo "Updating $EXAMPLE";
+
+    for VERSION in ${VERSIONS[*]}; do
+        DIR="$OLD_PWD/playground/$EXAMPLE/$VERSION";
+
+        if [[ -d "$DIR" ]]; then
+            cd "$DIR";
+            ncu -u;
+            npm i;
+        fi;
+    done;
 done;
 
 cd "$OLD_PWD";
