51273ca21f768addcdabe70c6d677d10108136aa
Eryk Napierała <eryk.piast@gmail.com>
2020-09-21 21:09:26 +0200

    chore(testing): add Create React App example

---
+++ playground/create-react-app/b/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import moment from 'moment';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        We have {moment()} now.
+      </header>
+    </div>
+  );
+}
+
+export default App;
